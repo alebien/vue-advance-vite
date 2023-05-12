709f542a00e52af595a84e1bb3b273183971c0ad
sxw <s9x9w9@163.com>
2023-05-12 15:06:53 +0800

    up vue from 3.2 to 3.3

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 /*
  * @Date: 2023-05-10 11:57:35
  * @LastEditors: sxw s9x9w9@163.com
- * @LastEditTime: 2023-05-10 12:20:28
+ * @LastEditTime: 2023-05-12 15:05:14
  * @FilePath: \vue-advance-vite\src\main.ts
  */
-import { createApp } from 'vue'
+import { createApp, inject } from 'vue'
 import router from './router';
 import { createPinia } from 'pinia';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App);
+app.provide('name', 'sxw');
+
+router.beforeEach((to, from, next) => {
+    console.log(inject('name'));
+    next();
+})
+app.use(router).use(createPinia()).mount('#app');
+
